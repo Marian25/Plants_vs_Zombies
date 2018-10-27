ad742d9249cf149033aedb64bc7b6431419faa7a
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-27 13:36:48 +0300

    Background Music For Levels Added

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    [SerializeField] AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+
+    private void Awake()  {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        AudioClip thisLevelMusic = levelMusicChangeArray[level];
+
+        if (thisLevelMusic) {
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+    }
+}
