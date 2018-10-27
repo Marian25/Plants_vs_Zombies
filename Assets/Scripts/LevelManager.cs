0d4dce78a9d6094728affd5d9576a1f714323b89
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-27 12:53:18 +0300

    Splash Screen & Main Menu Screen Added

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    // parameters
+    [SerializeField] float autoLoadNextLevelAfter;
+
+    private void Start() {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    public void LoadLevel(string name) {
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest() {
+        Application.Quit();
+    }
+
+    public void LoadNextLevel() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
