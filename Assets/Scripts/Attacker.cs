a383f117bd86600e2d2443753b5ff509d94634ad
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-29 21:05:17 +0200

    Attackers Animation on Collision Setup

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -2,15 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (Rigidbody2D))]
 public class Attacker : MonoBehaviour {
 
-    [SerializeField] [Range(-1f, 1.5f)] float currentSpeed = .5f;
-
-	// Use this for initialization
-	void Start () {
-        Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D>();
-        myRigidBody.isKinematic = true;
-	}
+    private float currentSpeed = .5f;
+    private GameObject currentTarget;
 	
 	// Update is called once per frame
 	void Update () {
@@ -26,9 +22,13 @@ public class Attacker : MonoBehaviour {
         currentSpeed = speed;
     }
 
+    // Called from the animator at time of actual blow
     public void StrikeCurrentTarget(float damage) {
         Debug.Log(name + " caused damage: " + damage);
     }
 
+    public void Attack(GameObject obj) {
+        currentTarget = obj;
+    }
 
 }
