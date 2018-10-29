606bb33062852a755acb54db5920eae1c39e520f
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-29 20:31:01 +0200

    Animation Events for Lizard & Fox Added

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour {
 
-    [SerializeField] [Range(-1f, 1.5f)] float walkSpeed = .5f;
+    [SerializeField] [Range(-1f, 1.5f)] float currentSpeed = .5f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +14,21 @@ public class Attacker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(name + " trigger enter");
     }
+
+    public void SetCurrentSpped(float speed) {
+        currentSpeed = speed;
+    }
+
+    public void StrikeCurrentTarget(float damage) {
+        Debug.Log(name + " caused damage: " + damage);
+    }
+
+
 }
