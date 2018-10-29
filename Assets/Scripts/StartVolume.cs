5a56d477438bf682b16577239b3bc8f15ab98271
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-29 18:21:49 +0200

    Prefabs Characters & Scenes Improvements

---
+++ Assets/Scripts/StartVolume.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartVolume : MonoBehaviour {
+
+    private MusicManager musicManager;
+
+	// Use this for initialization
+	void Start () {
+        musicManager = FindObjectOfType<MusicManager>();
+        if (musicManager) {
+            musicManager.ChangeVolume(PlayerPrefsManager.GetMasterVolume());
+        } else {
+            Debug.LogWarning("No music manager found in Start Scene");
+        }
+	}
+
+}
