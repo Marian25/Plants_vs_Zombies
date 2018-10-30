3e14f72acdbdd59056ef9ef15a51a3226cc26533
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-30 15:14:53 +0200

    Using Stars as Currency

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Defender : MonoBehaviour {
 
-	// Only being used as a tag for now
+    private StarDisplay starDisplay;
+
+    private void Start()
+    {
+        starDisplay = FindObjectOfType<StarDisplay>();
+    }
+
+    // Only being used as a tag for now
+
+    public void AddStars(int amount) {
+        starDisplay.AddStars(amount);
+    }
 
 }
