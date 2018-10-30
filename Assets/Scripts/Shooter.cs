7f0b4f754c5022575e4e3109e0c4bea940e5e0c1
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-30 14:50:57 +0200

    Shooters Detect Attackers

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,14 +8,60 @@ public class Shooter : MonoBehaviour {
     [SerializeField] GameObject gun;
 
     private GameObject projectileParent;
+    private Animator animator;
+    private Spawner myLaneSpawner;
 
     private void Start()
     {
-        projectileParent = GameObject.Find("Projectiles");
+        animator = FindObjectOfType<Animator>();
 
+        // Create a parent if necessary
+        projectileParent = GameObject.Find("Projectiles");
         if (!projectileParent) {
             projectileParent = new GameObject("Projectiles");
         }
+
+        SetMyLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerAheadInLane()) {
+            animator.SetBool("isAttacking", true);
+        } else {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    // Look through all spawners, and set myLaneSpawner if found
+    private void SetMyLaneSpawner() {
+        Spawner[] allSpawners = FindObjectsOfType<Spawner>();
+
+        foreach(Spawner thisSpawner in allSpawners) {
+            if (Mathf.Abs(thisSpawner.transform.position.y - transform.position.y) < 0.00001f) {
+                myLaneSpawner = thisSpawner;
+                return;
+            }
+        }
+
+        Debug.LogError(name + "can't find spawner in lane");
+    }
+
+    private bool IsAttackerAheadInLane() {
+        // Exit if no attackers in lane
+        if (myLaneSpawner.transform.childCount <= 0) {
+            return false;
+        }
+
+        // If there are attackers, are they ahead?
+        foreach(Transform attacker in myLaneSpawner.transform) {
+            if (attacker.transform.position.x > transform.position.x) {
+                return true;
+            }
+        }
+
+        // Attacker in lane, but behind
+        return false;
     }
 
     private void Fire() {
