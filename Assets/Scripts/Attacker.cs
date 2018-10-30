3992b0be4e9c3f8eef6659ed24a4c2cf59c8b928
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-30 09:33:09 +0200

    Health Component Added to All Characters

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -7,10 +7,20 @@ public class Attacker : MonoBehaviour {
 
     private float currentSpeed = .5f;
     private GameObject currentTarget;
-	
-	// Update is called once per frame
-	void Update () {
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
         transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
+
+        if (!currentTarget) {
+            animator.SetBool("isAttacking", false);
+        }
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -24,7 +34,15 @@ public class Attacker : MonoBehaviour {
 
     // Called from the animator at time of actual blow
     public void StrikeCurrentTarget(float damage) {
-        Debug.Log(name + " caused damage: " + damage);
+
+        if (currentTarget) {
+            Health health = currentTarget.GetComponent<Health>();
+
+            if (health) {
+                health.DealDamage(damage);
+            }
+        }
+
     }
 
     public void Attack(GameObject obj) {
