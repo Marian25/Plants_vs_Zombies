066436846dbd6c2e64d9a081538948fa1a539a6d
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-31 20:02:37 +0200

    Play Testing & Tuning

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,6 +1,7 @@
-﻿ using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Button : MonoBehaviour {
 
@@ -8,10 +9,18 @@ public class Button : MonoBehaviour {
     public static GameObject selectedDefender;
 
     private Button[] buttonArray;
+    private Text costText;
 
 	// Use this for initialization
 	void Start () {
         buttonArray = FindObjectsOfType<Button>();
+        costText = GetComponentInChildren<Text>();
+
+        if (!costText) {
+            Debug.LogWarning(name + " has no cost text!");
+        }
+
+        costText.text = defenderPrefab.GetComponent<Defender>().starCost.ToString();
 	}
 	
 	// Update is called once per frame
